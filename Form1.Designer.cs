f089a4fc0af54f270a53b50f6a092f2514315dd5
Tristan Carpenter <77842798+trisiegt@users.noreply.github.com>
2022-09-18 18:02:34 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.floor = new System.Windows.Forms.PictureBox();
+            this.trex = new System.Windows.Forms.PictureBox();
+            this.obstacle1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.scoreText = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.floor)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trex)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstacle1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // floor
+            // 
+            this.floor.BackColor = System.Drawing.Color.Lime;
+            this.floor.Location = new System.Drawing.Point(-12, 452);
+            this.floor.Name = "floor";
+            this.floor.Size = new System.Drawing.Size(652, 50);
+            this.floor.TabIndex = 0;
+            this.floor.TabStop = false;
+            // 
+            // trex
+            // 
+            this.trex.Image = global::WindowsFormsApp1.Properties.Resources.running;
+            this.trex.Location = new System.Drawing.Point(130, 367);
+            this.trex.Name = "trex";
+            this.trex.Size = new System.Drawing.Size(44, 60);
+            this.trex.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.trex.TabIndex = 1;
+            this.trex.TabStop = false;
+            // 
+            // obstacle1
+            // 
+            this.obstacle1.Image = global::WindowsFormsApp1.Properties.Resources.obstacle_1;
+            this.obstacle1.Location = new System.Drawing.Point(373, 412);
+            this.obstacle1.Name = "obstacle1";
+            this.obstacle1.Size = new System.Drawing.Size(21, 39);
+            this.obstacle1.TabIndex = 2;
+            this.obstacle1.TabStop = false;
+            this.obstacle1.Tag = "obstacle";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources.obstacle_2;
+            this.pictureBox1.Location = new System.Drawing.Point(534, 400);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Tag = "obstacle";
+            // 
+            // scoreText
+            // 
+            this.scoreText.AutoSize = true;
+            this.scoreText.Font = new System.Drawing.Font("Pusab", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreText.Location = new System.Drawing.Point(13, 13);
+            this.scoreText.Name = "scoreText";
+            this.scoreText.Size = new System.Drawing.Size(110, 21);
+            this.scoreText.TabIndex = 4;
+            this.scoreText.Text = "Score - 0";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameEvent);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Pusab", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(213, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(393, 69);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "One cactus = 1 point\r\nOne group with 2 cacti = 1 point\r\nThree cacti - 2 points";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(618, 475);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.scoreText);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.obstacle1);
+            this.Controls.Add(this.trex);
+            this.Controls.Add(this.floor);
+            this.Name = "Form1";
+            this.Text = "T Rex Endless Runner";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.floor)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trex)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstacle1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox floor;
+        private System.Windows.Forms.PictureBox trex;
+        private System.Windows.Forms.PictureBox obstacle1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label scoreText;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
